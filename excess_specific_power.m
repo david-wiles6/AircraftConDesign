189370233e92acf72157f8621a718549be2a4a4a
david-wiles6 <31745593+david-wiles6@users.noreply.github.com>
2025-02-27 15:31:06 -0500

    Change Sp excess power calcs to more real values

--- excess_specific_power.m
+++ excess_specific_power.m
@@ -1,17 +1,26 @@
-%calculate and plot excess specific power
+%excess specific power stuff
 
 % (V*(T*cos(alpha) - D)/W
 % T - based on altitude and speed
 % alpha
 % W 50% fuel
-altitude = linspace(0, 30000, 100);%ft
-M = linspace(0, 1.6, 100); % mach
+% V done
+% D done
+altitude = [0 15000];%ft
+M = 0.9; % mach
 S = 300; %ft^2
+T = [26000 35000 35000];
+alpha = 3; %deg
 V = zeros(length(M), length(altitude));
-for i = 1:length(M)
-    for j = 1:length(altitude)
-        V = M(i)*sqrt(gamma*R*calcTempRankine(altitude(j)));
-        D = 0.5*getCD(V, altitude(j))*getRhoSlugs(altitude(j))*S*V^2;
+W = 30000;
 
+for i = 1:length(T)
+    for j = 1:length(altitude)
+        V = M*sqrt(gamma*R*calcTempRankine(altitude(j)));
+        D = 0.5*getCD(M, altitude(j))*getRhoSlugs(altitude(j))*S*V^2;
+        T = T(i);
+        Spex = V*(T*cosd(alpha)-D)/W;
     end
 end
+
+Spex
