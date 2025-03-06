a1d9aa4d88dd9e3f6866b98ceb32b341200471d7
david-wiles6 <czt8kd@virginia.edu>
2025-03-06 01:42:45 -0500

    add more phases to weight est algorithm

--- InitialWeightEst.m
+++ InitialWeightEst.m
@@ -1,8 +1,15 @@
 %Initial Weight Estimation for an Aircraft
 
 function [WeightEst, W_e] = InitialWeightEst(W_pay, A, C, steps_id, steps_L, steps_V, LDRatio, TSFC, W0_Guess) 
-%steps id: 1 for distance based cruise, 2 for distance based dash, 3 for
-%time based loiter
+%steps id: 0 for climb/accelerate (V is speed to accelerate to, L is climb height)1 for distance based cruise, 
+%2 for distance based dash, 3 for time based loiter 4 for combat
+
+%{
+ climb/accelerate fuel burn: 
+h_e = h+V^2/(2g)
+f_s = 
+
+%}
 epsilon = 100000;
 while(epsilon>100)
     R_empty = A*W0_Guess^(C);
@@ -10,12 +17,16 @@ while(epsilon>100)
     R_climbaccel = .985;
     R_total = 1;
     for ii = 1:length(steps_id)
-        if steps_id(ii) == 1
+        if steps_id(ii) == 0
+            R(ii) = 1;
+        elseif steps_id(ii) == 1
             R(ii) = exp((-steps_L(ii)*0.8*TSFC/3600)/(steps_V(ii)*LDRatio*.866));
         elseif steps_id(ii) == 2
             R(ii) = exp((-steps_L(ii)*0.8*TSFC/3600)/(steps_V(ii)*LDRatio*.866));
         elseif steps_id(ii) == 3
             R(ii) = exp(-steps_L(ii)*TSFC/3600/LDRatio);
+        elseif steps_id(ii) == 4
+            R(ii) = 1;
         end
         R_total = R(ii)*R_total;
     end
