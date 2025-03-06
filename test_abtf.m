792ab3d121df98a83da97d5515c0fe693e508fb9
david-wiles6 <czt8kd@virginia.edu>
2025-03-06 02:05:03 -0500

    Change to m instead of ft

--- test_abtf.m
+++ test_abtf.m
@@ -1,7 +1,7 @@
 %test_abtf.m
 
 M = linspace(0.6, 1.6, 100);
-Z = linspace(0, 40000, 100);
+Z = linspace(0, 40000/3.281, 100);
 
 for i = 1:length(M)
     for j = 1:length(Z)
@@ -9,4 +9,4 @@ for i = 1:length(M)
     end
 end
 
-surf(M, Z, T/4.448)
+surf(M, Z*3.281, T/4.448)
