a7db0c2c74f06ce01c6956a6aa509d9f1aba1f0b
june-wiles <czt8kd@virginia.edu>
2025-03-06 15:11:57 -0500

    Edit Afterburning turbofan

--- afterburningTF.m
+++ afterburningTF.m
@@ -1,4 +1,4 @@
-function [TSFC, Thrust, TSFCAB, ThrustAB] = afterburningTF(M, z, radius, T_04MAX)
+function [u_en, TSFC, Thrust, TSFCAB, ThrustAB] = afterburningTF(M, z, radius, T_04MAX, P_rc, B)
 % M, Z, radius, T04_MAX
 % Engine Simulation ew
 A_in = pi*(radius^2);
@@ -11,25 +11,23 @@ R = 287; %R/MM
 % Speed
 g = 1.4;
 u = M*sqrt(g*R*Ta);
-m_dota = rhoa*A_in*u;
+%m_dota = rhoa*A_in*u;
 % Inlet
-T_02 = Ta*(1 + (g-1)/2*M^2);
+T_02 = Ta*(1 + (g-1)/2*(M^2));
 n_d = 1;
 P_02 = Pa*(1 + n_d*(T_02/Ta-1))^(g/(g-1));
 
 % Fan
-B = 0.3;
 P_rf = 1.5;
 P_08 = P_02 * P_rf;
-n_f = 1;
+n_f = .85;
 T_08 = T_02 * (1 + (1/n_f)*(P_rf^((g-1)/g)-1));
 u_ef = sqrt(2*n_f*g/(g-1)*R*T_08*(1-(Pa/P_08)^((g-1)/g)));
 
 % Compressor
 %Input: P_rc
-P_rc = 26;
 P_03 = P_rc*P_02;
-n_c = 1;
+n_c = .85;
 T_03 = T_02*(1+(1/n_c)*(P_rc^((g-1)/g)-1));
 
 % Burner
@@ -41,7 +39,7 @@ f = (T_04/T_03-1)/(QR/(C_p*T_03)-T_04/T_03);
 P_04 = P_03;
 
 % Turbine
-n_t = 1;
+n_t = .9;
 T_05 = T_04 - C_p/(n_c*n_t*C_p)*(T_03-T_02)-B*(T_08-T_02);
 P_05 = P_04*(1-1/n_t*(1-T_05/T_04))^(g/(g-1));
 
@@ -55,14 +53,16 @@ T_06AB = Tmax_AB;
 P_06AB = P_05;
 
 % Nozzle
-n_n = 1;
+n_n = .98;
 u_en = sqrt(2*n_n*g/(g-1)*R*T_06*(1-(Pa/P_06)^((g-1)/g)));
 u_enAB = sqrt(2*n_n*g/(g-1)*R*T_06AB*(1-(Pa/P_06AB)^((g-1)/g)));
 
 %Thrust
-Thrust = m_dota*((1+f)*u_en+B*u_ef-(1+B)*u);
-ThrustAB = m_dota*((1+f)*u_enAB+B*u_ef-(1+B)*u);
-TSFC = f*m_dota/Thrust;
+Thrust = ((1+f)*u_en+B*u_ef-(1+B)*u); %N/kg
+%Thrust = m_dota*((1+f)*u_en+B*u_ef-(1+B)*u);
+%ThrustAB = m_dota*((1+f)*u_enAB+B*u_ef-(1+B)*u);
+ThrustAB = 
+TSFC = f/SpT;
 TSFCAB = f*m_dota/ThrustAB;
 
 end
--- test_abtf.m
+++ test_abtf.m
@@ -1,12 +1,20 @@
 %test_abtf.m
 
-M = linspace(0.6, 1.6, 100);
+M = linspace(0.9, 1.6, 100);
 Z = linspace(0, 40000/3.281, 100);
+%Z = 0;
+%M = 0.9;
 
 for i = 1:length(M)
     for j = 1:length(Z)
-        [~, T(i, j), ~, ~] = afterburningTF(M(i), Z(j), 0.5, 1922);
+        [u_e(i, j), ~, T(i, j), ~, ~] = afterburningTF(M(i), Z(j), 0.5, 1922);
     end
 end
 
-surf(M, Z*3.281, T/4.448)
+%[~, TSFC, T, ~, ~] = afterburningTF(M, Z, 0.5, 1922, 26, 0.3);
+% TSFC in SI is kg/N/s
+%TSFC_hr = TSFC*3600;
+%TSFCImperial = TSFC_hr*9.80665
+%TImperial = T/4.448
+contour(M, Z, T)
+
